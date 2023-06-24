c311f3e030fc5b9f1b7ee91e3f578f14055c3b60
naonao-aaa <naonao1814naonao@gmail.com>
2023-06-24 14:45:59 +0900

    Link 名前付きルート

--- resources/js/Pages/InertiaTest.vue
+++ resources/js/Pages/InertiaTest.vue
@@ -7,5 +7,5 @@ import { Link } from '@inertiajs/inertia-vue3';
 Inertiaテストです。<br>
 <a href="/">aタグ経由です</a><br>
 <Link href="/">Link経由です</Link><br>
-
+<Link :href="route('inertia.index')">名前付きルートの確認です</Link><br>
 </template>
