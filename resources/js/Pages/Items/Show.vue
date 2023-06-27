5cbaba6945babda575c75d037e6772fc727f4ef1
naonao-aaa <naonao1814naonao@gmail.com>
2023-06-27 11:27:43 +0900

    商品編集

--- resources/js/Pages/Items/Show.vue
+++ resources/js/Pages/Items/Show.vue
@@ -1,6 +1,6 @@
 <script setup>
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
-import { Head } from '@inertiajs/vue3';
+import { Head, Link } from '@inertiajs/vue3';
 import { nl2br } from '@/common'
 
 defineProps({
@@ -23,7 +23,7 @@ defineProps({
                     <div class="p-6 bg-white border-b border-gray-200">
 
                         <section class="text-gray-600 body-font relative">
-                          <form @submit.prevent="storeItem">
+                          
                             <div class="container px-5 py-8 mx-auto">
 
                             <div class="lg:w-1/2 md:w-2/3 mx-auto">
@@ -65,13 +65,13 @@ defineProps({
                                 </div>
 
                                 <div class="p-2 w-full">
-                                  <button class="flex mx-auto text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">商品登録</button>
+                                  <Link as="button" :href="route('items.edit', { item: item.id })" class="flex mx-auto text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">編集する</Link>
                                 </div>
 
                               </div>
                             </div>
                           </div>
-                          </form>
+                          
                         </section>
                     </div>
                 </div>
