711fde6f3299aba681307cee332fc842ba08fd97
naonao-aaa <naonao1814naonao@gmail.com>
2023-06-28 08:46:09 +0900

    Customer ページネーション

--- resources/js/Pages/Customers/Index.vue
+++ resources/js/Pages/Customers/Index.vue
@@ -2,9 +2,10 @@
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
 import { Head, Link } from '@inertiajs/vue3';
 import FlashMessage from '@/Components/FlashMessage.vue'
+import Pagination from '@/Components/Pagination.vue'
 
 defineProps({
-  customers: Array
+  customers: Object
 })
 
 </script>
@@ -38,7 +39,7 @@ defineProps({
                                   </tr>
                                 </thead>
                                 <tbody>
-                                  <tr v-for="customer in customers" :key="customer.id">
+                                  <tr v-for="customer in customers.data" :key="customer.id">
                                     <td class="border-b-2 border-gray-200 px-4 py-3">{{ customer.id }}</td>
                                     <td class="border-b-2 border-gray-200 px-4 py-3">{{ customer.name }}</td>
                                     <td class="border-b-2 border-gray-200 px-4 py-3">{{ customer.kana }}</td>
@@ -49,6 +50,9 @@ defineProps({
                             </div>
 
                           </div>
+
+                          <Pagination class="mt-6" :links="customers.links"></Pagination>
+
                         </section>
                     </div>
                 </div>
