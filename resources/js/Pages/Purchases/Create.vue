63743da3af6d0966c312659ae801a7eccc6c3d4a
naonao-aaa <naonao1814naonao@gmail.com>
2023-06-29 15:50:47 +0900

    micromodal.js

--- resources/js/Pages/Purchases/Create.vue
+++ resources/js/Pages/Purchases/Create.vue
@@ -5,6 +5,7 @@ import { Head } from '@inertiajs/vue3';
 import { onMounted, reactive, ref, computed } from 'vue'
 import { Inertia } from '@inertiajs/inertia'
 import { getToday } from '@/common'
+import MicroModal from '@/Components/MicroModal.vue'
 
 const props = defineProps({
   'errors': Object,
@@ -87,6 +88,7 @@ const quantity = [ "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
 
                                 <div class="p-2 w-full">
                                   <div class="relative">
+                                    <MicroModal />
                                     <label for="customer" class="leading-7 text-sm text-gray-600">会員名</label>
                                     <select name="customer" v-model="form.customer_id" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
                                       <option v-for="customer in customers" :value="customer.id" :key="customer.id">
