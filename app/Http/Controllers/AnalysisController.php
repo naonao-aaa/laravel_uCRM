546cb58abf384ed7e8252f10efd1836ff4bab4ee
naonao-aaa <naonao1814naonao@gmail.com>
2023-07-09 13:32:15 +0900

    日別売上のクエリ作成・取得確認

--- app/Http/Controllers/AnalysisController.php
+++ app/Http/Controllers/AnalysisController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 use App\Models\Order;
+use Illuminate\Support\Facades\DB;
 
 class AnalysisController extends Controller
 {
@@ -13,13 +14,17 @@ class AnalysisController extends Controller
         $startDate = '2022-08-01';
         $endDate = '2022-08-31';
 
-        $period = Order::betweenDate($startDate, $endDate)
+        $subQuery = Order::betweenDate($startDate, $endDate)
+        ->where('status', true)
         ->groupBy('id')
-        ->selectRaw('id, sum(subtotal) as total, customer_name, status, created_at')
-        ->orderBy('created_at')
-        ->paginate(50);
+        ->selectRaw('id, sum(subtotal) as totalPerPurchase, DATE_FORMAT(created_at, "%Y%m%d") as date');
 
-        // dd($period);
+        $data = DB::table($subQuery)
+        ->groupBy('date')
+        ->selectRaw('date, sum(totalPerPurchase) as total' )
+        ->get();
+
+        // dd($data);
 
         return Inertia::render('Analysis');
     }
